46a49ad5698019bdb051318db96bb1b540ef3375
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-23 00:00:29 +0000

    Contact, Invoices & Form Pages

--- react-admin/src/scenes/global/SideBar.jsx
+++ react-admin/src/scenes/global/SideBar.jsx
@@ -22,7 +22,7 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
     return (
-        <MenuItem active={selected === title} style={{ color: colors.grey[100] }} onclick={() => setSelected(title)} icon={icon}>
+        <MenuItem active={selected === title} style={{ color: colors.grey[100] }} onClick={() => setSelected(title)} icon={icon}>
             <Typography>{title}</Typography>
             <Link to={to} />
         </MenuItem>

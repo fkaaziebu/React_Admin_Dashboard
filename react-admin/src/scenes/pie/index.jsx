f7fb829f59e911c70e6ec0b44b3c2223ed1aa918
fkaaziebu <frederickaziebu1998@gmail.com>
2022-12-27 23:35:06 +0000

    PieChart Alignment

--- react-admin/src/scenes/pie/index.jsx
+++ react-admin/src/scenes/pie/index.jsx
@@ -6,8 +6,15 @@ const Pie = () => {
     return (
         <Box m="20px">
             <Header title="Pie Chart" subtitle="Simple Pie Chart" />
-            <Box height="75vh">
-                <PieChart />
+            <Box
+                display="grid"
+                gridTemplateColumns="repeat(12, 1fr)"
+                gridAutoRows="100vh"
+                gap="20px"
+            >
+                <Box gridColumn="span 12" display="flex" alignItems="center" justifyContent="center">
+                    <PieChart />
+                </Box>
             </Box>
         </Box>
     )
